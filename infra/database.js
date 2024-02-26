ee04c0b3a88989fa3b228bb14c4f6c2b379cc405
Arthur Ribeiro <59943281+tucokk@users.noreply.github.com>
2024-02-26 17:50:09 +0000

    handles database ssl connection

--- infra/database.js
+++ infra/database.js
@@ -7,6 +7,7 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     port: process.env.POSTGRES_PORT,
     database: process.env.POSTGRES_DB,
+    ssl: process.env.NODE_ENV == "development" ? false : true,
   });
 
   console.log("Credenciais do Postgres: ", {
