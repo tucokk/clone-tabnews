ed582a722c6f1e42b00bb04a37ffd88ff1a3d6e5
Arthur Ribeiro <59943281+tucokk@users.noreply.github.com>
2024-02-26 12:21:38 +0000

    `database.js` more robust to error handling

--- infra/database.js
+++ infra/database.js
@@ -8,10 +8,17 @@ async function query(queryObject) {
     port: process.env.POSTGRES_PORT,
     database: process.env.POSTGRES_DB,
   });
+
   await client.connect();
-  const result = await client.query(queryObject);
-  await client.end();
-  return result;
+
+  try {
+    const result = await client.query(queryObject);
+    return result;
+  } catch (error) {
+    console.log(error);
+  } finally {
+    await client.end();
+  }
 }
 
 export default {
